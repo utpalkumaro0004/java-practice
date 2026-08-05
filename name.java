9f3de8035d7bd5d25ebc327d6df5c02bfd271fef
Utpal Kumar <utpalkumar040@gmail.com>
2026-08-06 04:33:35 +0530

    update

--- name.java
+++ name.java
@@ -1,7 +1,9 @@
 public class name {
     public static void main(String[] args) {
-        System.out.print("my name is utpal kumar");
-        System.out.print("from bihar");
-        System.out.print("i learn java ");
+        System.out.print("my name is utpal kumar   ");
+        System.out.print("from bihar   ");
+        System.out.println("i learn java ");
+        System.out.println("utpal");
+        System.out.println("kumar");
     }
 }
